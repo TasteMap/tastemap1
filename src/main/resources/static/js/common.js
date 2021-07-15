3a853637a5a2245aba62e29336ffaf676aa43f5f
HEEJU-a <heeju9923@naver.com>
2021-07-16 00:32:36 +0900

    login, join 모달창 띄우기해결(css고쳐야함, 백엔드 덜함)-희주

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,14 +1,39 @@
-// 로그인 모달창
-var modalLogOpen = document.querySelector('#loginModal');
-var modalLogClose = document.querySelector('.login-container')
+const modalLogOpen = document.querySelector('#loginModal');
+const modalJoinOpen = document.querySelector('#joinModal');
+const bg = document.querySelector('.bg');
+const loginForm = document.querySelector('#login');
+const joinForm = document.querySelector('#join');
 
-const open = () => {
-    document.querySelector(".login-container").classList.remove("hidden");
-}
+const close_btn = document.querySelector('.close_btn');
 
-const close = () => {
-    document.querySelector(".login-container").classList.add("hidden");
-}
+//로그인 모달창
+modalLogOpen.addEventListener('click', () => {
+    bg.style.display = 'block';
+    loginForm.style.display = 'block';
+})
 
-modalLogOpen.addEventListener('click', open);
-modalLogClose.addEventListener('click', close);
+close_btn.addEventListener('click', () => {
+    bg.style.display = 'none';
+    loginForm.style.display = 'none';
+})
+
+bg.addEventListener('click', () => {
+    bg.style.display = 'none';
+    loginForm.style.display = 'none';
+})
+
+//join 모달창
+modalJoinOpen.addEventListener('click', () => {
+    bg.style.display = 'block';
+    joinForm.style.display = 'block';
+})
+
+close_btn.addEventListener('click', () => {
+    bg.style.display = 'none';
+    joinForm.style.display = 'none';
+})
+
+bg.addEventListener('click', () => {
+    bg.style.display = 'none';
+    joinForm.style.display = 'none';
+})
