1f741bd5e4dcbc971c7efcb98ed7545015f7938d
Jiyeong-github <81175088+jiyeong-github@users.noreply.github.com>
2021-07-17 22:16:25 +0900

    0717 - detail페이지 예약 datepicker 적용

--- src/main/resources/static/js/board/detail.js
+++ src/main/resources/static/js/board/detail.js
@@ -1,7 +1,4 @@
-$(function() {
-    $( "#datepicker" ).datepicker({ minDate: 0});
-});
-    $(function() {
+ $(function() {
     //input을 datepicker로 선언
     $("#datepicker").datepicker({
         dateFormat: 'yy-mm-dd' //달력 날짜 형태
@@ -28,3 +25,6 @@ $(function() {
     //초기값을 오늘 날짜로 설정해줘야 합니다.
     $('#datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
 });
+// $(function() {
+//    $( "#datepicker" ).datepicker({ minDate: 0});
+// });
