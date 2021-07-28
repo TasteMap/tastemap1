0cc38cd65abad6dd89f4c90c850c8d91466ea401
AnWonYoung <ko1529@naver.com>
2021-07-28 20:38:48 +0900

    디테일 지도 + 길찾기 연동(원영)

---
+++ src/main/resources/static/js/board/detail.js
@@ -0,0 +1,68 @@
+$(function() {
+    //input을 datepicker로 선언
+    $("#datepicker").datepicker({
+        dateFormat: 'yy-mm-dd' //달력 날짜 형태
+        ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+        ,showMonthAfterYear:true // 월- 년 순서가아닌 년도 - 월 순서
+        ,changeYear: true //option값 년 선택 가능
+        ,changeMonth: true //option값  월 선택 가능
+        ,currentText: '오늘 날짜'  // 오늘 날짜로 이동하는 버튼 패널
+        ,closeText: '닫기' // 닫기 버튼 패널 dateFormat: "yy-mm-dd",
+        ,showAnim: "slide" //애니메이션을 적용한다.
+        ,showMonthAfterYear: true // 월, 년순의 셀렉트 박스를 년,월 순으로 바꿔준다.
+        ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
+        ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
+        ,buttonImageOnly: true //버튼 이미지만 깔끔하게 보이게함
+        ,buttonText: "선택" //버튼 호버 텍스트
+        ,yearSuffix: "년" //달력의 년도 부분 뒤 텍스트
+        ,monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 텍스트
+        ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip
+        ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 텍스트
+        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 Tooltip
+        //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전),minDate: "-5Y"
+        ,maxDate: "+5m" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+    });
+    //초기값을 오늘 날짜로 설정해줘야 합니다.
+    $('#datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
+});
+// $(function() {
+//    $( "#datepicker" ).datepicker({ minDate: 0});
+// });
+
+// 각 주소마다 지도 띄우기
+var address = $(".rsad").html();
+var title = $(".title").html();
+var container = document.getElementById('map');
+var options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3
+};
+
+var map = new kakao.maps.Map(container, options);
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch(address, function(result, status) {
+    // 정상적으로 검색이 완료됐으면
+    if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        kakao.maps.event.addListener(marker, 'click', function() {
+            //마커 클릭 시 길찾기로 연동
+            window.location.href='https://map.kakao.com/link/to/' + title + ',' + coords.getLat() + ',' + coords.getLng();
+        });
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">길찾기</div>'
+        });
+        infowindow.open(map, marker);
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    }
+});
